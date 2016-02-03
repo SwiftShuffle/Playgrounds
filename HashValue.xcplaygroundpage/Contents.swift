e0f3d1d2edd02fd678b78b74163ce40ccd1e733f
Seán Labastille <git@lsr.li>
2016-02-03 22:18:34 +0100

    Add expression values

--- HashValue.xcplaygroundpage/Contents.swift
+++ HashValue.xcplaygroundpage/Contents.swift
@@ -1,21 +1,21 @@
 //: Illustration of `hashValue` behaviour and how this interacts with a `Set`'s `first` property
-//: See [this `Swift: Shuffle All` post](https://sen-labastille-fql1.squarespace.com/swift-shuffle-all/2016/2/3/struct-set-var-first-get-) for details.
+//: See [this `Swift: Shuffle All` post](https://sean.systems/swift-shuffle-all/2016/2/3/struct-set-var-first-get-) for details.
 
-"a".hashValue
-"b".hashValue
-"z".hashValue
-"ab".hashValue
-"zz".hashValue
-"aaa".hashValue
+"a".hashValue // 4799450059485595655
+"b".hashValue // 4799450059485595656
+"z".hashValue   // 4799450059485595728
+"ab".hashValue  // 4799450059485098232
+"zz".hashValue  // 4799450059484804847
+"aaa".hashValue // 4799450059707603575
 
 //: Observe how the behaviour changes as a function of the `minimumCapacity`
 var set = Set<String>(minimumCapacity: 1023)
-set.insert("a")
-set.insert("zz")
-set.insert("ab")
-set.insert("aaa")
-set.insert("z")
-set.insert("b")
+set.insert("a") // {"a"}
+set.insert("zz") // {"zz", "a"}
+set.insert("ab") // {"ab", "zz", "a"}
+set.insert("aaa") // {"aaa", "ab", "zz", "a"}
+set.insert("z") // {"aaa", "z", "ab", "zz", "a"}
+set.insert("b") // {"aaa", "z", "ab", "zz", "b", "a"}
 
 
-set.first
+set.first // "aaa"
